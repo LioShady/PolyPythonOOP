16516230b93c8f229c0af3624a16ef4859ceb223
Denis Pelipeychenko <pelipejchenko.da@edu.spbstu.ru>
2024-01-29 23:55:57 +0300

    initial commit

--- Лабораторная 3/main.py
+++ Лабораторная 3/main.py
@@ -1,31 +1,45 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self._name = name
+        self._author = author
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Книга {self._name}. Автор {self._author}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        pass
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        pass
 
 
-class PaperBook:
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
+        super().__init__(name, author)
         self.pages = pages
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return super().__str__() + f". Количество страниц {self.pages}"
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
+        super().__init__(name, author)
         self.duration = duration
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return super().__str__() + f". Длительность {self.duration}"
